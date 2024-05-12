9d3d4cd1ccf7fe8b803fafa09691aadf4135fdb8
Ricarte13 <viniciusdiv@hotmail.com>
2024-05-12 16:31:09 -0300

    criando função exibirTextoNaTela

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,8 +1,10 @@
-let title = document.querySelector('h1');
-title.innerHTML = 'Jogo do número secreto';
+function exibirTextoNaTela(tag, text) {
+    let field = document.querySelector(tag);
+    field.innerHTML = text;
+}
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
-let paragraph = document.querySelector('p');
-paragraph.innerHTML = 'Escolha um número entre 1 e 10';
 
 function verificarChute(){
     console.log('O botão foi clicado!');
